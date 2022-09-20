96f65ec4ce143d79287b73f087ced5be32242d65
hptdev <62223472+hpt-dev@users.noreply.github.com>
2022-09-20 22:36:35 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+string firstName = " Harry ";
+string lastName = " Potter";
+string message = "His name is";
+
+// new string created, message not modified
+message += firstName + lastName + " and he is a wizard"; 
+
+// use string builder, faster, no additiona memory created, can return ToString()
+StringBuilder sb = new StringBuilder();
+sb.Append("His name is");
+sb.Append(" Harry ");
+sb.Append(" Potter");
+sb.Append(" and he is a wizard");
+sb.ToString();
---
+++ StringBenchmark.cs
@@ -0,0 +1,46 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// using BenchmarkDotNet.Running;
+// var _ = BenchmarkRunner.Run<StringBenchmark>();
+
+
+[SimpleJob]
+[RPlotExporter]
+[MemoryDiagnoser]
+public class StringBenchmark
+{
+
+        [GlobalSetup]
+        public void Setup()
+        {
+
+        }
+
+        [Benchmark]
+        public void RegularString() 
+        {
+            string s = string.Empty;
+            for (int i = 0; i < 1000; i++)
+            {
+                s += i.ToString() + " ";
+            }
+        }
+
+
+        [Benchmark]
+        public void StringBuilder()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 1000; i++)
+            {
+                sb.Append(i);
+                sb.Append(' ');
+            }
+        }
+    }
